249fa0dc94a5049627856641f3aeee6878d444b9
n1o2h <aythmadnhylt@gmail.com>
2024-04-28 14:11:17 -0700

    task 5

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,14 +4,10 @@ import os
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
-from flask_cors import CORS
 
 
 # creating a Flask app
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
-
-
 app.register_blueprint(app_views, url_prefix="/api/v1")
 
 
@@ -24,22 +20,6 @@ def page_not_found(e):
 def page_not_found(e):
     message = e.description
     return message, 400
-
-
-@app.teardown_appcontext
-def close(ctx):
-    storage.close()
-
-
-if os.getenv("HBNB_API_HOST"):
-    host = os.getenv("HBNB_API_HOST")
-else:
-    host = "0.0.0.0"
-
-if os.getenv("HBNB_API_PORT"):
-    port = int(os.getenv("HBNB_API_PORT"))
-else:
-    port = 5000
 
 
 if __name__ == "__main__":
