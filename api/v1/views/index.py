1fbe7e6dd82118f285999cfb95d8e2f364404022
n1o2h <aythmadnhylt@gmail.com>
2024-04-28 13:55:04 -0700

    task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -10,3 +10,19 @@ def status():
     return {
         "status": "OK",
     }
+@app_views.route("/stats", strict_slashes=False, methods=["GET"])
+def stats():
+    amenities = storage.count(Amenity)
+    cities = storage.count(City)
+    places = storage.count(Place)
+    reviews = storage.count(Review)
+    states = storage.count(State)
+    users = storage.count(User)
+    return {
+        "amenities": amenities,
+        "cities": cities,
+        "places": places,
+        "reviews": reviews,
+        "states": states,
+        "users": users,
+    }
