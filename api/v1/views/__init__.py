96c7bbfe7535eb3db5405d4f176a44e2139a8c89
n1o2h <aythmadnhylt@gmail.com>
2024-04-28 14:31:43 -0700

    task 6

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """sharing app_views Blueprint"""
 
-from flask import Blueprint
-app_views = Blueprint('app_views', __name__)
 from api.v1.views.index import *
 from api.v1.views.states import *
+from flask import Blueprint
+app_views = Blueprint('app_views', __name__)
